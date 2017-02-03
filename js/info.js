583f3fae1ed4ef3c8800601ac2f46da2ff209f7a
hygull <rishikesh0014051992@gmail.com>
2017-02-03 17:05:55 +0530

    3 added multiple required pages with primary common contents

--- js/info.js
+++ js/info.js
@@ -1,12 +1,4 @@
 <!--
-
-function pythoFrankTimer(){
-	var d=new Date();
-	document.getElementById("span_timer").innerHTML=d.toLocaleTimeString()
-	document.getElementById('span_resolution').innerHTML= "Resolution : "+screen.width+"x"+screen.height;
-	document.getElementById('span_host').innerHTML="Host : "+location.hostname;
-	document.getElementById('span_protocol').innerHTML="Protocol : "+location.protocol;
-}
 function showElements(){
 	document.getElementById("div_list_group").style.display="block";
 	document.getElementById("a_show").style.display="none";
@@ -16,7 +8,13 @@ function hideElements(){
 	document.getElementById("div_list_group").style.display="none";
 	document.getElementById("a_show").style.display="block";
 }
-setInterval(pythoFrankTimer,1000);
+
+function info() {
+	showElements();
+	document.getElementById('span_resolution').innerHTML= "Resolution : "+screen.width+"x"+screen.height;
+	document.getElementById('span_host').innerHTML="Host : "+location.hostname;
+	document.getElementById('span_protocol').innerHTML="Protocol : "+location.protocol;
+}
 
 //-->
 
---
+++ js/timer.js
@@ -0,0 +1,7 @@
+<!--
+function pythoFrankTimer(){
+	var d=new Date();
+	document.getElementById("span_timer").innerHTML=d.toLocaleTimeString()
+}
+setInterval(pythoFrankTimer,1000);
+//-->
