c898afe740c39e15f36b833fd89abd739aa5f655
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2019-07-06 22:34:29 +0530

    added animation, and decorated with image rotaions

---
+++ pages/demo1/js/icons.js
@@ -0,0 +1,15 @@
+var loginIcons = [
+	'https://img.icons8.com/bubbles/344/brunette-girl-phone-call.png',
+	'https://img.icons8.com/bubbles/344/accept-or-decline-man-call.png',
+	'https://img.icons8.com/bubbles/2x/end-call-male.png',
+	'https://img.icons8.com/clouds/344/phone.png',
+	'https://image.flaticon.com/icons/png/512/1891/1891028.png'
+]
+
+
+
+function getIcons(type='login', count=5) {
+	if(type=='login') {
+		return loginIcons
+	}
+}
--- pages/demo1/js/login.js
+++ pages/demo1/js/login.js
@@ -4,3 +4,37 @@ function login(event) {
 
 	sweetAlertSuccess('Login successfull', 'Click OK/Cancel to visit your accounts/home page')
 }
+
+
+function changeIcons(secs=3000) {
+	let icons = getIcons(type='login', count=5)
+	let i = 0
+	console.log(icons)
+
+
+	console.log('Setting ', icons[i])
+		
+	setInterval(function() {
+		// $('#login-img').attr('src', icons[i]).show('slow') 
+
+		// (1st)
+		// $('#login-img').attr('src', icons[i])
+
+		// (2nd) https://stackoverflow.com/questions/2744579/jquery-animate-on-an-image-replacement
+		let image = $('#login-img')
+		image.fadeOut('slow', function () {
+        	image.attr('src', icons[i])
+        	image.fadeIn('slow');
+    	});
+
+		if(i == icons.length -1) {
+			i = 0
+		} 
+		
+
+		i++
+		console.log('ICON ', icons[i], ' set')
+	}, secs)
+}
+
+changeIcons()
