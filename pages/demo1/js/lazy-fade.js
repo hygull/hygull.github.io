e53be902bd14aef7efd0d71c023827b4ef972490
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2019-07-13 15:02:13 +0530

    added images to index

---
+++ pages/demo1/js/lazy-fade.js
@@ -0,0 +1,8 @@
+// http://jquery.eisbehr.de/lazy/#about
+    $(function() {
+        $('.lazy').lazy({
+          effect: "fadeIn",
+          effectTime: 3000,
+          threshold: 0
+        });
+    });   
